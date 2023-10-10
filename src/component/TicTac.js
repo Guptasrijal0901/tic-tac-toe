2550828caf43d45b929bd860f0e5e0109680892b
srijal gupta <guptasrijal0901@gmail.com>
2023-10-10 10:09:39 +0530

    day2

--- src/component/TicTac.js
+++ src/component/TicTac.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./TicTac.css"
 import circle from "./circle.png"
 import cross from "./cross.png"
 
+let data = ["", "", "", "", "", "", "", "", "" ];
+
+let [count, setcount]= useState(0);
+let [lock, setlock] = useState(false);
+const toggle = (w, num)=>{
+if (count%2===0) {
+  e.target.innerHTML = `img src = ${cross} `
+  data[num]= "x"
+  setcount(++count);
+} else {
+  e.target.innerHTML = `img src = ${circle} `
+  data[num]= "0"
+  setcount(++count);
+}
+  }
 const TicTac = () => {
+  
   return (
     <div className='container'>
       <h1 className='title'> Tic Tac Toe Game In <span> React</span> </h1>
